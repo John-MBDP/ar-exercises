de124c83be465ffa66402d496f5a76350289d4a2
John <johnartuz@hotmail.com>
2022-01-31 22:12:00 -0500

    Display error message when store name is not provided

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,5 @@
 class Store < ActiveRecord::Base
-  has_many :employees,
-
+  has_many :employees
+  validates :annual_revenue, numericality: { greater_than: 0}
+  validates :name  lanegth: {min: 3}
 end
